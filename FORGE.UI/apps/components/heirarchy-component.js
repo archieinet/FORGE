7cf86b82ef7b009d77f6de733ae07fc3799e47c0
Archie <archieinet@gmail.com>
2017-10-29 12:09:27 -0500

    push apps

---
+++ FORGE.UI/apps/components/heirarchy-component.js
@@ -0,0 +1,22 @@
+﻿(function() {
+    'use strict';
+
+    function controller($log) {
+        var hc = this;
+        hc.isCollapsed = true;
+        
+
+
+
+        //$log.info('heirachy connected');
+    }; //controller;
+
+    angular
+        .module('app_heirarchy', [])
+        .component('heirarchyComponent', {
+            templateUrl: 'apps/views/heirarchy-component.html',
+            controller: ['$log', controller],
+            controllerAs: 'hc'
+        });
+
+})();
